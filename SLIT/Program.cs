9f14ae1599a13425c68443200f8e6268ba39490d
Valentin Lorentz <valentin.lorentz@gmail.com>
2011-03-07 13:19:41 +0100

    First commit

---
+++ SLIT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.SPOT;
+using System.Threading;
+using Microsoft.SPOT.Hardware;
+using GHIElectronics.NETMF.FEZ;
+
+namespace MFConsoleApplication1
+{
+    public class Program {
+        public static void Main()
+        {
+
+        }
+    }
+}
